9672d512f79755eb824da46be49f441b60f2179b
SebastianUrbano <john.urbano@correo.icesi.edu.co>
2020-12-08 18:43:34 +0000

    changes on app

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+echo "desplegando base de datos"
+echo "."
+echo ".."
+kubectl create -f redis_deploy.yml
+echo "..."
+echo "Base de datos redis desplegada"
 echo "Desplegando app web"
 echo "."
 echo ".."
 kubectl create -f python_deploy.yml
 echo "..."
 echo "App web en python desplegada"
-echo "Desplegando base de datos"
-echo "."
-echo ".."
-kubectl create -f redis_deploy.yml
-echo "..."
-echo "Base de datos redis desplegada"
 echo "La aplicación está lista para ser probada en la ip publica del balanceador que se muestra a continuación"
 kubectl get svc
